a325a2798a6210f28a1921f0635b3b343c91e97b
MSKTArcanine <marcantoine1999@live.fr>
2025-10-24 01:16:22 +0200

    Linting

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,15 +1,15 @@
 export class NotFoundError extends Error {
-    constructor(resource:string){
-        super(`${resource} not found`);
-        this.name = "NotFoundError";
-        this.statusCode = 404;
-    }
+  constructor(resource:string) {
+    super(`${resource} not found`);
+    this.name = 'NotFoundError';
+    this.statusCode = 404;
+  }
 }
 
 export class InvalidContentType extends Error {
-    constructor(){
-        super('Wrong request data');
-        this.name = "InvalidContentTypeError";
-        this.statusCode = 405;
-    }
+  constructor() {
+    super('Wrong request data');
+    this.name = 'InvalidContentTypeError';
+    this.statusCode = 405;
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -3,69 +3,67 @@ import http from 'node:http';
 const PORT = Number(process.env.PORT || 5173);
 
 const server = http.createServer((req, res) => {
-    const { method, url } = req;
-    const passedUrl = new URL(url!, `http://${req.headers.host}`);
-    const pathname = passedUrl.pathname;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  const { method, url } = req;
+  const passedUrl = new URL(url!, `http://${req.headers.host}`);
+  const { pathname } = passedUrl;
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+  const contentType = req.headers['content-type'] ?? '';
+  const isJson = contentType.includes('application/json');
+  const POST_MAX = 1000000;
+  let size = 0;
 
-    const contentType = req.headers['content-type'] ?? '';
-    const isJson = contentType.includes('application/json');
-    const POST_MAX = 1000000;
-    let size = 0;
+  if (method === 'OPTIONS') {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+  if (method === 'GET' && pathname === '/') {
+    console.log('OUI');
+    res.writeHead(200, {
+      'content-type': 'text/html; charset=utf-8',
+    });
+    res.end('<h1>test</h1>');
+    return;
+  }
 
-    if (method === 'OPTIONS') {
-        res.writeHead(204);
-        res.end();
-        return;
-    }
-    if (method === 'GET' && pathname === '/') {
-        console.log('OUI');
-        res.writeHead(200, {
-            'content-type': 'text/html; charset=utf-8'
-        });
-        res.end('<h1>test</h1>');
-        return;
-    }
-
-    if (method === 'POST' && pathname === '/'){
-      console.log('POST OUI');
-      if(!isJson){
-        res.writeHead(415, {"Content-Type":"application/json; charset=utf-8"});
-        res.end(JSON.stringify({data:`Unsupported type : use application/json. Used : ${contentType}`}));
-        return;
-      }
-      let body = '';
-      req.on('data', chunk => {
-        size += chunk.length;
-        if(size > POST_MAX){
-          res.writeHead(413, {"Content-Type":"application/json; charset=utf-8"});
-          res.end(JSON.stringify({error:'Too large, size max 1Mo'}));
-          req.destroy();
-          return;
-        }
-        body += chunk;
-      });
-      req.on('end', () => {
-        try {
-          const data = JSON.parse(body);
-          console.log(data);
-          res.writeHead(204);
-          res.end();
-        }catch(e){
-          console.error(e);
-          res.writeHead(400, {'content-type':'application/json; charset=utf-8'});
-          res.end(JSON.stringify({error:'Invalid JSON'}));
-        }
-      });
-      req.on('error', () => {
-        res.writeHead(400, {'Content-Type':'application/json; charset=utf-8'});
-        res.end(JSON.stringify({error:'Bad request'}));
-      })
+  if (method === 'POST' && pathname === '/') {
+    console.log('POST OUI');
+    if (!isJson) {
+      res.writeHead(415, { 'Content-Type': 'application/json; charset=utf-8' });
+      res.end(JSON.stringify({ data: `Unsupported type : use application/json. Used : ${contentType}` }));
       return;
     }
+    let body = '';
+    req.on('data', (chunk) => {
+      size += chunk.length;
+      if (size > POST_MAX) {
+        res.writeHead(413, { 'Content-Type': 'application/json; charset=utf-8' });
+        res.end(JSON.stringify({ error: 'Too large, size max 1Mo' }));
+        req.destroy();
+        return;
+      }
+      body += chunk;
+    });
+    req.on('end', () => {
+      try {
+        const data = JSON.parse(body);
+        console.log(data);
+        res.writeHead(204);
+        res.end();
+      } catch (e) {
+        console.error(e);
+        res.writeHead(400, { 'content-type': 'application/json; charset=utf-8' });
+        res.end(JSON.stringify({ error: 'Invalid JSON' }));
+      }
+    });
+    req.on('error', () => {
+      res.writeHead(400, { 'Content-Type': 'application/json; charset=utf-8' });
+      res.end(JSON.stringify({ error: 'Bad request' }));
+    });
+  }
 });
 
 server.listen(PORT, () => {
