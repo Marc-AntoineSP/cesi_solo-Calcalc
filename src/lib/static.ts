154b382b017722b482c8af4790434e8057ecf048
MSKTArcanine <marcantoine1999@live.fr>
2025-10-26 23:47:24 +0100

    PROGFIX: test de fix. TODO: deplacement public a la racine

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -1,5 +1,6 @@
-import fs from 'fs';
 import type { IncomingMessage, ServerResponse } from 'http';
+import fs from 'node:fs';
+import fss from 'node:fs/promises';
 import path from 'path';
 
 const MIME: Record<string, string> = {
@@ -18,24 +19,37 @@ export default function staticServing(publicDir: string) {
     pathname: string,
   ): Promise<boolean> {
     const method = req.method ?? 'GET';
-    const filePath = path.join(publicPath, pathname);
+    if (method !== 'GET' && method !== 'HEAD') return false; // QUE get et head
+    if (!pathname.startsWith('/public/')) return false; // QUE public
+
+    let relatifPath = pathname.slice('/public/'.length);
+    if (relatifPath === '' || relatifPath.endsWith('/')) relatifPath += 'index.html';
+
+    const filePath = path.join(publicPath, relatifPath);
+
     try {
-      const extension = path.extname(filePath).toLowerCase();
-      res.statusCode = 200;
-      res.setHeader('Content-Type', MIME[extension] ?? 'application/octet-stream');
-      if (method === 'HEAD') {
-        res.end(); return true;
-      }
-      const stream = await fs.createReadStream(filePath);
-      stream.on('error', () => {
-        res.statusCode = 500;
-        res.end('Read fail !');
-      });
-      stream.pipe(res);
-      return true;
+      await fss.access(filePath, fs.constants.R_OK);
     } catch (e) {
       console.error(e);
       return false;
     }
+
+    const extension = path.extname(filePath).toLowerCase();
+    res.statusCode = 200;
+    res.setHeader('Content-Type', MIME[extension] ?? 'application/octet-stream');
+
+    if (method === 'HEAD') {
+      res.end();
+      return true;
+    }
+    const stream = fs.createReadStream(filePath);
+    stream.on('error', () => {
+      if (!res.headersSent) {
+        res.statusCode = 500;
+        res.end('Read fail');
+      }
+    });
+    stream.pipe(res);
+    return true;
   };
 }
