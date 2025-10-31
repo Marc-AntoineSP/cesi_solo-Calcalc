12984b90dbeedfe1470b3017b37fd31dacce6a90
MSKTArcanine <marcantoine1999@live.fr>
2025-10-31 02:26:35 +0100

    FEAT: Controle du modal

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,1 +1,24 @@
-console.log('Test');
+const gridContainer = document.getElementById('gridContainer');
+const diag = document.getElementById('editModal');
+const form = document.getElementById('editForm');
+
+gridContainer.addEventListener('click', (e) => {
+  const submitBtn = e.target.closest('button[data-action="edit"]');
+  if (submitBtn === null) return;
+
+  const card = submitBtn.closest('article');
+  if (card === null) return;
+  const { id } = card.dataset;
+  const { name } = card.dataset;
+  const { description } = card.dataset;
+  const { calocent } = card.dataset;
+
+  form.elements.id.value = id;
+  form.elements.name.value = name;
+  form.elements.description.value = description;
+  form.elements.calocent.value = calocent;
+
+  diag.showModal();
+});
+
+console.log('fields in form:', [...form.elements].map((fElt) => fElt.name));
